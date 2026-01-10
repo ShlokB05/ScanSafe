1d38414b007d67c80d7aef2bd319281903b90b50
ShlokB05 <Shlokbohra@gmail.com>
2026-01-10 03:05:42 -0500

    .

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/static/",
   plugins: [react()],
-  base: "/static/",          
-  build: {
-    outDir: "dist",          
-    emptyOutDir: true
-  }
+  server: {
+    proxy: {
+      "/api": { target: "http://127.0.0.1:8000", changeOrigin: true, secure: false },
+    },
+  },
 });
