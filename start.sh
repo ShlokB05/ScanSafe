934784110ca71ddb26a5c7579bc5860b031d23ea
ShlokB05 <Shlokbohra@gmail.com>
2026-01-10 02:00:19 -0500

    start

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+exec gunicorn ScanSafe.wsgi:application \
+  --bind 0.0.0.0:${PORT:-8080} \
+  --timeout 300 \
+  --workers 4
